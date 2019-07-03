304d059e3d5a2c5b4ec237ae54f0d61be4d4fdab
dxdbl <31200331+dxdbl@users.noreply.github.com>
2019-07-04 01:02:48 +0800

    Update torc_ddl.sql

--- torc_ddl.sql
+++ torc_ddl.sql
@@ -1,14 +1,14 @@
 CREATE TABLE test.test3_orc (
 `id` int COMMENT "NULL",
 `name` string COMMENT "NULL" )
-COMMENT 测试表
+COMMENT "测试表"
 CLUSTERED BY (id) INTO 1 BUCKETS
 STORED AS ORC
 TBLPROPERTIES('transactional'='true');
 CREATE TABLE test.test2_orc (
 `id` int COMMENT "编号",
 `name` string COMMENT "NULL" )
-COMMENT 姓名
+COMMENT "姓名"
 CLUSTERED BY (id) INTO 1 BUCKETS
 STORED AS ORC
 TBLPROPERTIES('transactional'='true');
